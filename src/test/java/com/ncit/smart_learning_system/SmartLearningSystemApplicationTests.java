9cc4826005bb6014177803789edcc19b0f868e58
Sudip9825 <152831385+Sudip9825@users.noreply.github.com>
2026-06-02 10:29:11 +0545

    summary added

---
+++ src/test/java/com/ncit/smart_learning_system/SmartLearningSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ncit.smart_learning_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartLearningSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
