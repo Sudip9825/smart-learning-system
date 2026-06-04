5bd35d93059a2e2c6c1c9877585217eb8b3b9264
Sudip9825 <152831385+Sudip9825@users.noreply.github.com>
2026-06-04 11:37:57 +0545

    changes 1 delete

--- smart-learning-system/src/main/java/com/ncit/smart_learning_system/common/RateLimitInterceptor.java
+++
@@ -1,4 +0,0 @@
-package com.ncit.smart_learning_system.common;
-
-public class RateLimitInterceptor {
-}
--- smart-learning-system/src/main/java/com/ncit/smart_learning_system/exception/CustomExceptions.java
+++
@@ -1,4 +0,0 @@
-package com.ncit.smart_learning_system.exception;
-
-public class CustomExceptions {
-}
